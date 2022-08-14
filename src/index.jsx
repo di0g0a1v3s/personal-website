2ebeeb027cd8eb961b835425b4cadbe863c1c8c8
di0g0a1v3s <diogo-alves1998@hotmail.com>
2022-08-15 00:09:26 +0100

    service worker

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,5 +13,6 @@ root.render(
 );
 
 reportWebVitals();
+serviceWorkerRegistration.register();
 
 
